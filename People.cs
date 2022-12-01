2a07229722cb8100a283882ccb1b37a2985eecd9
ThaboBomane <thabobomane2@gmail.com>
2022-12-01 09:45:28 +0200

    Add project files.

---
+++ People.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Orientated
+{
+    class People
+    {
+        public string name;
+        public string race;
+        public string gender;
+        public int age;
+
+        public People(string PersonName, string PersonGender, string PersonRace, int PersonAge)
+        {
+
+            name = PersonName;
+            age = PersonAge;
+            gender = PersonGender;
+            race = PersonRace;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Object_Orientated;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        People Person1 = new People(" Thabo", " female", " black", 30);
+        People Person2 = new People(" john", " male", " black", 20);
+        People Person3 = new People(" Lerato", " female", " black", 35);
+
+        Console.WriteLine(Person1.name + " " + Person1.gender + " " + Person1.race + " " + Person1.age);
+        Console.WriteLine(Person2.name + " " + Person2.gender + " " + Person2.race + " " + Person2.age);
+        Console.WriteLine(Person3.name + " " + Person3.gender + " " + Person3.race + " " + Person3.age);
+
+    }
+}
